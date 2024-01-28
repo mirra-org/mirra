4e36b030acfbd5dbce497d6afd4f28d9f33bd7b8
Olocool17 <22843298+Olocool17@users.noreply.github.com>
2024-01-28 14:47:32 +0100

    Slightly optimise MACAddress::fromString

--- firmware/lib/LoRaModule/CommunicationCommon.cpp
+++ firmware/lib/LoRaModule/CommunicationCommon.cpp
@@ -1,7 +1,6 @@
 #include "CommunicationCommon.h"
 #include <algorithm>
 
-
 char* MACAddress::toString(char* string) const
 {
     snprintf(string, MACAddress::stringLength, "%02X:%02X:%02X:%02X:%02X:%02X", this->address[0], this->address[1], this->address[2], this->address[3],
@@ -11,9 +10,10 @@ char* MACAddress::toString(char* string) const
 
 MACAddress MACAddress::fromString(char* string)
 {
-    uint8_t address[6];
+    MACAddress mac{};
+    uint8_t* address = mac.getAddress();
     sscanf(string, "%02X:%02X:%02X:%02X:%02X:%02X", &address[0], &address[1], &address[2], &address[3], &address[4], &address[5]);
-    return MACAddress(address);
+    return mac;
 }
 const MACAddress MACAddress::broadcast{};
 char MACAddress::strBuffer[MACAddress::stringLength];
