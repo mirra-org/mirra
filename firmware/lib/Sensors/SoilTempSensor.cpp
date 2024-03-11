8c1f450e63678020e7645b3a28e459334829db17
Olocool17 <22843298+Olocool17@users.noreply.github.com>
2024-03-11 11:10:51 +0100

    Improve DallasTemperature usage

--- firmware/lib/Sensors/SoilTempSensor.cpp
+++ firmware/lib/Sensors/SoilTempSensor.cpp
@@ -6,8 +6,8 @@ void SoilTemperatureSensor::startMeasurement() { dallas.begin(); }
 SensorValue SoilTemperatureSensor::getMeasurement()
 {
 
-    DeviceAddress longWireThermometer;
-    dallas.getAddress(longWireThermometer, this->busIndex);
-    dallas.requestTemperaturesByAddress(longWireThermometer);
-    return SensorValue(getID(), dallas.getTempCByIndex(this->busIndex));
+    DeviceAddress addr;
+    dallas.getAddress(addr, this->busIndex);
+    dallas.requestTemperaturesByAddress(addr);
+    return SensorValue(getID(), dallas.getTempC(addr));
 }
