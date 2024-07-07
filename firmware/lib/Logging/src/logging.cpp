a52d92ed98f9dded390642a956d0d5ac25eed058
Olocool17 <22843298+Olocool17@users.noreply.github.com>
2024-07-08 00:55:22 +0200

    Update logging to work with new FS

--- firmware/lib/Logging/src/logging.cpp
+++ firmware/lib/Logging/src/logging.cpp
@@ -1,108 +1,25 @@
 #include "logging.h"
 
+using namespace mirra;
+
 Log Log::log{};
 
-void Log::generateLogfilePath(char* buffer, const struct tm& time) { strftime(buffer, 16, "/%Y-%m-%d.log", &time); }
-
-void Log::pruneLogfiles()
+constexpr std::string_view Log::levelToString(Level level)
 {
-    struct LogFile
+    switch (level)
     {
-        char path[16];
-        size_t size;
-
-        bool operator<(const LogFile& other) { return strcmp(this->path, other.path) < 0; }
-    };
-
-    std::vector<LogFile> logFiles;
-    logFiles.reserve(32);
-    size_t logfileSizeSum{0};
-    File root = LittleFS.open("/");
-    size_t logfilesCount{0};
-    while (true)
-    {
-        File file = root.openNextFile();
-        if (!file)
-            break;
-        const char* filePath{file.path()};
-        if (strstr(filePath, ".log"))
-        {
-            LogFile& logFile = logFiles.emplace_back();
-            strncpy(logFile.path, filePath, sizeof(logFile.path));
-            logFile.size = file.size();
-            logfileSizeSum += logFile.size;
-            logfilesCount++;
-        }
-        file.close();
+    case Level::ERROR:
+        return "ERROR: ";
+    case Level::INFO:
+        return "INFO: ";
+    case Level::DEBUG:
+        return "DEBUG: ";
     }
-    root.close();
-    if (logfilesCount < 1 || logfileSizeSum <= maxSize * 1024)
-        return;
-    std::sort(std::begin(logFiles), std::end(logFiles));
-    for (const auto& logFile : logFiles)
-    {
-        if (!LittleFS.remove(logFile.path))
-        {
-            this->error("Could not remove logfile '", logFile.path, "'.");
-        }
-        else
-        {
-            this->info("Removed old logfile '", logFile.path, "'.");
-            logfileSizeSum -= logFile.size;
-        }
-        if (logfileSizeSum <= maxSize * 1024)
-        {
-            this->info("Logfiles pruned to ", logfileSizeSum / 1024, "KiB");
-            return;
-        }
-    }
+    return "NONE: ";
 }
 
-void Log::openLogfile(const struct tm& time)
-{
-    char logfilePath[16];
-    generateLogfilePath(logfilePath, time);
-    if (LittleFS.exists(logfilePath))
-    {
-        this->logfile = LittleFS.open(logfilePath, "a");
-    }
-    else
-    {
-        this->logfile = LittleFS.open(logfilePath, "w", true);
-    }
-    this->logfileTime = time;
-    if (!this->logfile)
-    {
-        this->logfileEnabled = false;
-        this->error("Unable to open/create logfile! Log will disable logging to file.");
-    }
-}
+size_t Log::File::cutTail(size_t cutSize) {}
 
-void Log::manageLogfile(struct tm& time)
-{
-    if (!this->logfileEnabled)
-        return;
-    if ((this->logfileTime.tm_mday != time.tm_mday) || (this->logfileTime.tm_mon != time.tm_mon) || (this->logfileTime.tm_year != time.tm_year) ||
-        (!this->logfile))
-    {
-        if (this->logfile)
-            this->logfile.close();
-        openLogfile(time);
-        if (!this->logfile)
-            return;
-        pruneLogfiles();
-    }
-}
+void Log::init() { Log::log.file = Log::File(); }
 
-void Log::logfilePrint()
-{
-    if (!this->logfileEnabled)
-        return;
-    this->logfile.println(buffer);
-}
-
-void Log::close()
-{
-    this->logfile.close();
-    this->logfileEnabled = false;
-}
+void Log::close() { Log::log.~Log(); }
--- firmware/lib/Logging/src/logging.h
+++ firmware/lib/Logging/src/logging.h
@@ -1,14 +1,16 @@
-#ifndef __LOGGING_H__
-#define __LOGGING_H__
+#ifndef __MIRRA_LOGGING_H__
+#define __MIRRA_LOGGING_H__
 
+#include "../MIRRAFS/FS.h"
 #include <HardwareSerial.h>
-#include <LittleFS.h>
 #include <type_traits>
 
+namespace mirra
+{
 class Log
 {
 public:
-    enum Level : uint8_t
+    enum class Level : uint8_t
     {
         DEBUG,
         INFO,
@@ -17,35 +19,21 @@ public:
 
 private:
     Log() = default;
+    Log(const Log&) = delete;
+    Log(Log&&) = default;
+    Log& operator=(const Log&) = delete;
+    Log& operator=(Log&&) = default;
 
-    /// @brief Serial to print to.
-    HardwareSerial* logSerial{nullptr};
-    /// @brief Logging level of the logging module. Messages below this level will not be printed.
-    Level level{static_cast<uint8_t>(-1)};
+    class File final : public fs::FIFOFile
+    {
+        size_t cutTail(size_t cutSize);
 
-    /// @brief Whether logging to file is enabled.
-    bool logfileEnabled{false};
-    /// @brief Time struct of the currently loaded logging file.
-    tm logfileTime{0};
-    /// @brief Currently loaded logging file. Should be opened in append mode.
-    File logfile{};
-    /// @brief Maximum size (in KiB) that all logging files should occupy.
-    static constexpr size_t maxSize{1024};
+    public:
+        File() : FIFOFile("logs") {}
+    };
 
-    /// @brief Generates a logfile path from a time struct.
-    /// @param buffer Buffer to which to write the logfile path.
-    /// @param time Time for which to generate a logfile path.
-    void generateLogfilePath(char* buffer, const struct tm& time);
-    /// @brief Removes logfiles until maxSize is respected (oldest first).
-    void pruneLogfiles();
-    /// @brief Opens/creates a logfile with the given date.
-    /// @param time The date for which to open/create a logfile.
-    void openLogfile(const struct tm& time);
-    /// @brief Manages all file-related operations for a given date.
-    /// @param time The date from which to manage the filesystem.
-    void manageLogfile(struct tm& time);
-    /// @brief Prints the current buffer to the currently open logfile.
-    void logfilePrint();
+    /// @brief Currently loaded logging file.
+    File file;
 
     /// @brief Buffer in which the final string is constructed and printed from.
     char buffer[256]{0};
@@ -53,7 +41,7 @@ private:
     /// @tparam level The level displayed in the preamble.
     /// @param time The time displayed in the preamble.
     /// @return The length of the preamble that was printed.
-    template <Level level> size_t printPreamble(const tm& time);
+    template <Log::Level level> size_t printPreamble(const tm& time);
     /// @return String conversion from a level.
     static constexpr std::string_view levelToString(Level level);
     /// @return A format specifier string matched to the type argument.
@@ -61,34 +49,35 @@ private:
     template <class T> static constexpr std::string_view rawTypeToFormatSpecifier();
     /// @return A format string matched to the given type arguments.
     template <class... Ts> static constexpr auto createFormatString();
-    /// @brief Type-safe variadic print function. Uses compile-time format string instantiation to ensure safety in using printf.
+    /// @brief Type-safe variadic print function. Uses compile-time format string instantiation to
+    /// ensure safety in using printf.
     /// @param buffer Buffer to which to print.
     /// @param max Max number of characters to print to buffer.
-    template <class... Ts> void printv(char* buffer, size_t max, Ts... args);
-    /// @brief Prints to the logging buffer and forwards to (if enabled) the output serial and logfile.
+    template <class... Ts> size_t printv(char* buffer, size_t max, Ts&&... args);
+    /// @brief Prints to the logging buffer and forwards to (if enabled) the output serial and
+    /// logfile.
     /// @tparam level Log level of printed message.
-    template <Level level, class... Ts> void print(Ts... args);
+    template <Log::Level level, class... Ts> void print(Ts&&... args);
 
 public:
-    /// @param logSerial HardwareSerial object to print to.
-    void setSerial(HardwareSerial* logSerial) { this->logSerial = logSerial; }
-    /// @param level Level to set the logging module to. Messages below this level will not be printed.
-    void setLogLevel(Log::Level level) { this->level = level; }
-    /// @brief Enables or disables the logging to file functionality.
-    void setLogfile(bool enable = true) { this->logfileEnabled = enable; }
+    /// @brief Serial to print to.
+    HardwareSerial* serial{nullptr};
+    /// @brief Logging level of the logging module. Messages below this level will not be printed.
+    Level level{Level::INFO};
+    /// @brief Whether logging to file is enabled.
+    bool fileEnabled{false};
 
     /// @brief Singleton global log object
     static Log log;
+    template <class... Ts> static void debug(Ts... args) { log.print<Level::DEBUG>(args...); }
+    template <class... Ts> static void info(Ts... args) { log.print<Level::INFO>(args...); }
+    template <class... Ts> static void error(Ts... args) { log.print<Level::ERROR>(args...); }
 
-    template <class... Ts> static void debug(Ts... args) { log.print<DEBUG>(args...); }
-    template <class... Ts> static void info(Ts... args) { log.print<INFO>(args...); }
-    template <class... Ts> static void error(Ts... args) { log.print<ERROR>(args...); }
-
+    /// @brief Initialises the logging module.
+    static void init();
     /// @brief Closes the logging module, releasing the logging file.
-    void close();
+    static void close();
 
-    Log(const Log&) = delete;
-    Log& operator=(const Log&) = delete;
     ~Log() = default;
 };
 
@@ -101,43 +90,25 @@ template <Log::Level level> size_t Log::printPreamble(const tm& time)
     return timeLength + levelString.size() - 1;
 }
 
-constexpr std::string_view Log::levelToString(Level level)
-{
-    switch (level)
-    {
-    case Level::ERROR:
-        return "ERROR: ";
-    case Level::INFO:
-        return "INFO: ";
-    case Level::DEBUG:
-        return "DEBUG: ";
-    }
-    return "NONE: ";
-}
-
-// TODO: Clean up the type to format specifier compile-time functionality with proper SFINAE. This here works, but it's messy, requires two functions, and is
-// unruly to extend.
+// TODO: Clean up the type to format specifier compile-time functionality with proper SFINAE. This
+// here works, but it's messy, requires two functions, and is unruly to extend.
 
 template <class T> constexpr std::string_view Log::typeToFormatSpecifier()
 {
-    using rawType = std::remove_cv_t<std::remove_reference_t<T>>;
+    using rawType = std::decay_t<T>;
     if constexpr (std::is_enum_v<rawType>)
     {
-        using underlyingType = std::underlying_type_t<rawType>;
-        if constexpr (std::is_same_v<std::make_unsigned_t<underlyingType>, unsigned char>)
+        using rawType = std::underlying_type_t<rawType>;
+    }
+    if constexpr (std::is_same_v<std::make_unsigned_t<rawType>, unsigned char>)
+    {
+        if constexpr (std::is_signed_v<rawType>)
         {
-            if constexpr (std::is_signed_v<underlyingType>)
-            {
-                return rawTypeToFormatSpecifier<signed int>();
-            }
-            else
-            {
-                return rawTypeToFormatSpecifier<unsigned int>();
-            }
+            return rawTypeToFormatSpecifier<signed int>();
         }
         else
         {
-            return rawTypeToFormatSpecifier<underlyingType>();
+            return rawTypeToFormatSpecifier<unsigned int>();
         }
     }
     else
@@ -146,15 +117,24 @@ template <class T> constexpr std::string_view Log::typeToFormatSpecifier()
     }
 }
 
-template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<const char*>() { return "%s"; };
-template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<char*>() { return "%s"; };
-template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<signed int>() { return "%i"; };
-template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<unsigned int>() { return "%u"; };
-template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<long signed int>() { return "%i"; };
-template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<signed char>() { return "%i"; };
-template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<unsigned char>() { return "%u"; };
-template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<float>() { return "%f"; };
-template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<char>() { return "%c"; };
+template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<const char*>() { return "%s"; }
+template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<char*>() { return "%s"; }
+template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<signed int>() { return "%i"; }
+template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<unsigned int>()
+{
+    return "%u";
+}
+template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<long signed int>()
+{
+    return "%i";
+}
+template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<signed char>() { return "%i"; }
+template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<unsigned char>()
+{
+    return "%u";
+}
+template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<float>() { return "%f"; }
+template <> constexpr std::string_view Log::rawTypeToFormatSpecifier<char>() { return "%c"; }
 
 template <class... Ts> constexpr auto Log::createFormatString()
 {
@@ -168,23 +148,25 @@ template <class... Ts> constexpr auto Log::createFormatString()
     return fmt;
 }
 
-template <class... Ts> void Log::printv(char* buffer, size_t max, Ts... args)
+template <class... Ts> size_t Log::printv(char* buffer, size_t max, Ts&&... args)
 {
     constexpr auto fmt{createFormatString<Ts...>()};
-    snprintf(buffer, max, fmt.data(), args...);
+    return snprintf(buffer, max, fmt.data(), std::forward<Ts>(args)...);
 }
-template <Log::Level level, class... Ts> void Log::print(Ts... args)
+template <Log::Level level, class... Ts> void Log::print(Ts&&... args)
 {
     if (level < this->level)
         return;
     time_t ctime{time(nullptr)};
     tm* time{gmtime(&ctime)};
-    manageLogfile(*time);
-    size_t cur{printPreamble<level>(*time)};
-    size_t left{sizeof(buffer) - cur};
-    printv(&buffer[cur], left, args...);
-    logfilePrint();
-    logSerial->println(buffer);
+    size_t size{printPreamble<level>(*time)};
+    size += printv(&buffer[size], sizeof(buffer) - size, std::forward<Ts>(args)...);
+    buffer[size] = '\n';
+    size++;
+    file.push(buffer, size);
+    serial->write(buffer, size);
+}
+
 }
 
 #endif
