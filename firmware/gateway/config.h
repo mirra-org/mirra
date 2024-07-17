00c9266d461c049474ade9cc8838c10fb8c54fb3
Olocool17 <22843298+Olocool17@users.noreply.github.com>
2024-07-17 10:53:10 +0200

    Remove filepaths from config

--- firmware/gateway/config.h
+++ firmware/gateway/config.h
@@ -4,11 +4,6 @@
 // Pin definitions
 #include "pins_sensornode_rev5.h"
 
-// Filepaths
-#define NODES_FP "/nodes.dat"
-#define DATA_FP "/data.dat"
-#define DATA_TEMP_FP "/data_temp.dat"
-
 // WiFi settings
 #define WIFI_SSID "GontrodeWiFi2"
 #define WIFI_PASS "b5uJeswA"
@@ -19,10 +14,12 @@
 #define MQTT_PORT 1883
 #define MQTT_IDENTITY 0
 #define MQTT_PSK 0
-#define TOPIC_PREFIX "fornalab" // MQTT topic = `TOPIC_PREFIX` + '/' + `GATEWAY MAC` + '/' + `SENSOR MODULE MAC`
-#define MQTT_ATTEMPTS 5         // amount of attempts made to connect to MQTT server
-#define MQTT_TIMEOUT 1000       // ms, timeout to connect with MQTT server
-#define MAX_MQTT_ERRORS 3       // max number of MQTT publish errors after which uploading should be aborted
+#define TOPIC_PREFIX                                                                               \
+    "fornalab" // MQTT topic = `TOPIC_PREFIX` + '/' + `GATEWAY MAC` + '/' + `SENSOR MODULE MAC`
+#define MQTT_ATTEMPTS 5   // amount of attempts made to connect to MQTT server
+#define MQTT_TIMEOUT 1000 // ms, timeout to connect with MQTT server
+#define MAX_MQTT_ERRORS                                                                            \
+    3 // max number of MQTT publish errors after which uploading should be aborted
 
 // Communication and sensor settings
 
@@ -31,11 +28,14 @@
 // s, time between communication times for every nodes
 #define DEFAULT_COMM_INTERVAL (60 * 60)
 
-#define WAKE_BEFORE_COMM_PERIOD 5 // s, time before comm period when gateway should wake from deep sleep
-#define WAKE_COMM_PERIOD(X) ((X)-WAKE_BEFORE_COMM_PERIOD)
-#define LISTEN_COMM_PERIOD(X) ((X)-COMM_PERIOD_PADDING)
+#define WAKE_BEFORE_COMM_PERIOD                                                                    \
+    5 // s, time before comm period when gateway should wake from deep sleep
+#define WAKE_COMM_PERIOD(X) ((X) - WAKE_BEFORE_COMM_PERIOD)
+#define LISTEN_COMM_PERIOD(X) ((X) - COMM_PERIOD_PADDING)
 
-#define UPLOAD_EVERY 3 // amount of times the gateway will communicate with the nodes before uploading data to the server
+#define UPLOAD_EVERY                                                                               \
+    3 // amount of times the gateway will communicate with the nodes before uploading data to the
+      // server
 
 #define DEFAULT_SAMPLE_INTERVAL (20 * 60) // s, time between sensor sampling for every node
 #define DEFAULT_SAMPLE_ROUNDING (20 * 60) // s, round sampling time to nearest ...
