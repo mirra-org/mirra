e2409e0be1b92e0686633ebbd97dfd31e5cc3864
Olocool17 <22843298+Olocool17@users.noreply.github.com>
2024-09-13 12:18:44 +0200

    Revert flush on FIFOFile destructor

--- firmware/lib/MIRRAFS/FS.h
+++ firmware/lib/MIRRAFS/FS.h
@@ -155,7 +155,7 @@ protected:
 public:
     FIFOFile(FIFOFile&&) = default;
     FIFOFile& operator=(FIFOFile&&) = default;
-    virtual ~FIFOFile() { flush(); };
+    virtual ~FIFOFile() {};
 
     size_t getSize() const { return size; }
     size_t freeSpace() const;
