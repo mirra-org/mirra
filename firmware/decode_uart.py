82e496114ee6a4a6ce2f3b3b6faa42a4754e9b30
Olocool17 <22843298+Olocool17@users.noreply.github.com>
2024-11-05 12:53:06 +0100

    Update decode_uart script

--- firmware/decode_uart.py
+++ firmware/decode_uart.py
@@ -1,73 +1,55 @@
-# tijd (4) | number of sensor values  (1) | sensor id (1) | sensor value (4) | ...
-import datetime
-from collections import namedtuple
+import csv
+import json
+from argparse import ArgumentParser
+from datetime import datetime, timezone
+from pathlib import Path
 
-import numpy as np
-import pandas as pd
-import struct
-
-Sample = namedtuple("Sample", ["time", "n_values", "data"])
-SensorData = namedtuple("SensorData", ["id", "value"])
-
-samples = []
-
-sensor_ids = {
-    64: "esp-cam",
-    22: "light",
-    3: "soil-moisture",
-    4: "soil-temperature",
-    12: "air-temperature",
-    13: "relative-humidity",
-}
-
-with open("data.log") as f:
-    data = f.readlines()
-    data = [i.strip() for i in data]
-    data = [i for i in data if len(i) > 0]
-    data = ''.join(data)
-    data = [int(data[i:i+2], 16) for i in range(0,len(data),2)]
-
-    i = 0
-    while i < len(data):
-        # read time
-        t = bytes(data[i:i+4])
-        t = struct.unpack("<I", t)
-        t = datetime.datetime.fromtimestamp( t[0] )
-        i += 4
-        n_values = data[i]
-        i += 1
-        sensor_data = data[i:i+n_values*5]
-        i += n_values*5
-
-        sensor_data2 = []
-        for j in range(n_values):
-            s_id = sensor_data[5*j]
-            s_value = struct.unpack("<f", bytes(sensor_data[5*j+1:5*(j+1)]))[0]
-            sd = SensorData(id=s_id, value=s_value)
-            sensor_data2.append(sd)
+sensors = {}
 
 
-        sample = Sample(time=t, n_values=n_values, data=sensor_data2)
-        samples.append(sample)
+def get_sensors(path: Path):
+    with path.open() as infile:
+        sensors_json = json.load(infile)
+    for sensor in sensors_json:
+        sensors.update({sensor.id: (sensor.name, sensor.unit)})
 
-df = dict()
+
+def main(gateway_mac: str, out_path: Path, *in_paths: Path) -> None:
+    with out_path.open("w") as out:
+        out_csv = csv.writer(out)
+        for path in in_paths:
+            with path.open() as infile:
+                timestamp = None
+                node_mac = None
+                for line in infile:
+                    if len(line) == 0:
+                        node_mac = None
+                        continue
+                    if node_mac is None:
+                        split_line = line.split()[:3]
+                        node_mac = split_line[0]
+                        timestamp = " ".join(split_line[:1])
+                        timestamp = str(
+                            datetime.fromtimestamp(timestamp, tz=timezone.utc)
+                        )
+                    else:
+                        sensor_key, value = line.split()[:2]
+                        sensor_name, sensor_unit = sensors[sensor_key]
+                        out_csv.writerow(
+                            timestamp, gateway_mac, node_mac, sensor_name, sensor_unit
+                        )
 
 
-for i, sample in enumerate(samples):
-    if "time" in df:
-        df["time"].append(sample.time)
-    else:
-        df["time"] = [np.nan]*i + [sample.time]
-    for j, sd in enumerate(sample.data):
-        label = f"{sensor_ids[sd.id]}-{j}"
-        if label in df:
-            df[label].append(sd.value)
-        else:
-            df[label] = [np.nan]*i + [sd.value]
-    cl = np.max([len(df[j]) for j in df])
-    for j in df:
-        if len(df[j]) < cl:
-            df[j].append(np.nan)
-
-df = pd.DataFrame(df)
-df.to_csv("decoded_data.csv", index=False)
+if __name__ == "__main__":
+    parser = ArgumentParser(prog="mirra_decode")
+    parser.add_argument("file", nargs="+")
+    parser.add_argument("-g", "--gateway", required=True)
+    parser.add_argument("-s", "--sensors", required=True)
+    parser.add_argument("-o", "--out", default="decoded.csv")
+    parsed = parser.parse_args()
+    get_sensors(Path(parsed.sensors).resolve())
+    main(
+        parsed.gateway,
+        Path(parsed.out).resolve(),
+        *[Path(file).resolve() for file in parsed.file],
+    )
