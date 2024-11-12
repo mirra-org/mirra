9fde247f7581fc9657f05413789945cb3b14f4f5
Olocool17 <22843298+Olocool17@users.noreply.github.com>
2024-11-12 15:41:27 +0100

    Finalise decode_uart script

--- firmware/decode_uart.py
+++ firmware/decode_uart.py
@@ -11,32 +11,38 @@ def get_sensors(path: Path):
     with path.open() as infile:
         sensors_json = json.load(infile)
     for sensor in sensors_json:
-        sensors.update({sensor.id: (sensor.name, sensor.unit)})
+        sensors.update({sensor["id"]: (sensor["name"], sensor["unit"])})
 
 
 def main(gateway_mac: str, out_path: Path, *in_paths: Path) -> None:
-    with out_path.open("w") as out:
+    with out_path.open("a") as out:
         out_csv = csv.writer(out)
         for path in in_paths:
             with path.open() as infile:
                 timestamp = None
                 node_mac = None
                 for line in infile:
-                    if len(line) == 0:
+                    if len(line) <= 1:
                         node_mac = None
                         continue
                     if node_mac is None:
                         split_line = line.split()[:3]
                         node_mac = split_line[0]
-                        timestamp = " ".join(split_line[:1])
-                        timestamp = str(
-                            datetime.fromtimestamp(timestamp, tz=timezone.utc)
-                        )
+                        timestamp = " ".join(split_line[1:])
+                        timestamp = str(datetime.fromisoformat(timestamp))
                     else:
                         sensor_key, value = line.split()[:2]
-                        sensor_name, sensor_unit = sensors[sensor_key]
+                        sensor_name, sensor_unit = sensors[int(sensor_key)]
                         out_csv.writerow(
-                            timestamp, gateway_mac, node_mac, sensor_name, sensor_unit
+                            [
+                                timestamp,
+                                str(datetime.fromtimestamp(0, tz=timezone.utc)),
+                                gateway_mac,
+                                node_mac,
+                                value,
+                                sensor_name,
+                                sensor_unit,
+                            ]
                         )
 
 
