c371c16c1bfd5266fb45a4f891337105a4102918
Olocool17 <22843298+Olocool17@users.noreply.github.com>
2024-12-08 00:10:18 +0100

    Drastically improve db performance for timestamp queries

--- web/mirra_backend/data/measurement.py
+++ web/mirra_backend/data/measurement.py
@@ -8,21 +8,23 @@ from .modelbase import SqlAlchemyBase
 
 class Measurement(SqlAlchemyBase):
     __tablename__ = "measurements"
+    __table_args__ = {"sqlite_with_rowid": False}
 
-    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
-    timestamp: orm.Mapped[int]
-    insertion_timestamp: orm.Mapped[int]
-    value: orm.Mapped[float]
+    timestamp: orm.Mapped[int] = orm.mapped_column(primary_key=True)
 
     node_id: orm.Mapped[int] = orm.mapped_column(
-        sa.ForeignKey("modules.id", ondelete="CASCADE")
+        sa.ForeignKey("modules.id", ondelete="CASCADE"), primary_key=True
     )
     node: orm.Mapped["Node"] = orm.relationship(back_populates="measurements_list")
 
     sensor_id: orm.Mapped[int] = orm.mapped_column(
-        sa.ForeignKey("sensors.id", ondelete="CASCADE")
+        sa.ForeignKey("sensors.id", ondelete="CASCADE"), primary_key=True
     )
     sensor: orm.Mapped["Sensor"] = orm.relationship(back_populates="measurements_list")
+
+    insertion_timestamp: orm.Mapped[int]
+
+    value: orm.Mapped[float]
 
     @property
     def timestamp_iso(self) -> str:
